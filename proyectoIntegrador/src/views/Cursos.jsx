ecc788133fcdafd73f2f66f3a68c2b12b5dd296d
cvrolina-torres <carolinanoemi.t@gmail.com>
2024-02-15 03:58:32 -0300

    adición componente portada_cursos

--- proyectoIntegrador/src/views/Cursos.jsx
+++ proyectoIntegrador/src/views/Cursos.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
+import Cursos_portada from '../components/sections/cursos_portada'
 
 export const Cursos = () => {
   return (
-
+    <>
     <Navbar/>
+    <Cursos_portada/>
+    </>
   )
 }
+
