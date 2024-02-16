abc26817d4b11b184ae4eeb54a4dbeecb7bc0486
SoothTheRat <xblashxxzapata@gmail.com>
2024-02-16 01:05:11 -0300

    vista-mensajes-user

---
+++ proyectoIntegrador/src/components/curso-progreso/Casilla.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Casilla.css'
+
+function Casilla() {
+  return (
+    <div className='container-casilla' >
+        <div className='row'>
+            <div className='col'>
+                <h5>Lorem</h5>
+            </div>
+            
+            <div className='col'>
+                <button className='boton-curso' >Ver mensaje completo</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Casilla
