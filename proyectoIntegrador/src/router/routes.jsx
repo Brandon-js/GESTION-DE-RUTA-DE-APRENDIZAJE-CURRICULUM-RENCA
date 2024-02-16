089482aa28f5ff981b5c80032c70d184cfc24623
Bryan Poblete <bryanpobletereyes@gmail.com>
2024-02-16 04:37:54 -0300

    Solo es una prueba del Login

--- proyectoIntegrador/src/router/routes.jsx
+++ proyectoIntegrador/src/router/routes.jsx
@@ -5,13 +5,15 @@ import Registro from '../views/Registro.jsx';
 import Cursos from '../views/Cursos.jsx';
 import DashBoard from '../views/DashBoard.jsx'
 import DashMensaje from '../views/DashMensaje.jsx';
+import Login from '../components/login/Login.jsx'
+
 const router = createBrowserRouter([
     {path:'/', element: <Home />},
     {path:'/registro', element: <Registro />},
     {path:'/catalogo', element: <Catalogo />},
     {path:'/cursos', element: <Cursos />},
-    {path:'/dashboard-user', element: <DashBoard/>}
-    
+    {path:'/dashboard-user', element: <DashBoard/>},
+    {path:'/login', element: <Login/>},
 ]);
 
 export default router
