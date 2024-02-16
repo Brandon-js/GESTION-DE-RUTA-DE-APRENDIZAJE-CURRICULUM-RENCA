32e0f1fbe6083e001fda16bf6cd469f9ac959c95
uviboo <rodubat@gmail.com>
2024-02-16 00:34:59 -0300

    Revert "Merge pull request #11 from Brandon-js/Cursos-react"
    This reverts commit cd6d881f1f117148c73373664ad2281c60ee594c, reversing
    changes made to 6adf5c5c2310b83d20af68dc14fedafc4b785bcc.

--- proyectoIntegrador/src/components/botones/Boton_4.jsx
+++
@@ -1,9 +0,0 @@
-const Boton_4 = () => {
-  return (
-    <>
-      <button className="btn-custom">Continuar</button>
-    </>
-  );
-};
-
-export default Boton_4;
--- proyectoIntegrador/src/router/routes.jsx
+++ proyectoIntegrador/src/router/routes.jsx
@@ -1,16 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../views/Home";
-import Catalogo from "../views/Catalogo";
-import Registro from "../views/Registro.jsx";
-import Cursos from "../views/Cursos.jsx";
-import Curso from "../views/Curso.jsx";
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../views/Home'
+import Catalogo from '../views/Catalogo';
+import Registro from '../views/Registro.jsx';
+import Cursos from '../views/Cursos.jsx';
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/registro", element: <Registro /> },
-  { path: "/catalogo", element: <Catalogo /> },
-  { path: "/cursos", element: <Cursos /> },
-  { path: "/curso", element: <Curso /> },
+    {path:'/', element: <Home />},
+    {path:'/registro', element: <Registro />},
+    {path:'/catalogo', element: <Catalogo />},
+    {path:'/cursos', element: <Cursos />},
+    
 ]);
 
-export default router;
+export default router
