06e334f77262a5c6ff54b46f911551dc815d9d92
Bryan Poblete <bryanpobletereyes@gmail.com>
2024-02-23 11:00:26 -0300

    Modificando navbar responsive

--- Front-end/src/views/Catalogo.jsx
+++ Front-end/src/views/Catalogo.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 import Catalogo_portada from '../components/sections/Catalogo_portada'
+import Breadcrumb from '../components/breadcrumb/Breadcrumb'
+import Card_1 from '../components/cards/Card_1'
 
 const Catalogo = () => {
   return (
     <>
     <Navbar/>
     <Catalogo_portada/>
-    
+    <Breadcrumb />
+    <Card_1 />
+    <Card_1 />
+    <Card_1 />
+    <Card_1 />
+    <Card_1 />
+    <Card_1 />
+    <Card_1 />
+    <Card_1 />
     </>
   )
 }
