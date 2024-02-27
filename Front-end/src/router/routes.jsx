aa7ed4e9548b56f0212c540d3f5e69f93b350b71
Javiera Muñoz <153769615+uviboo@users.noreply.github.com>
2024-02-27 07:16:05 -0300

    Revert "FAQ front terminado"

--- Front-end/src/router/routes.jsx
+++ Front-end/src/router/routes.jsx
@@ -9,7 +9,6 @@ import Navbar from '../components/navbar/Navbar.jsx';
 import DashPerfil from '../views/DashPerfil.jsx';
 import MisCursos from '../views/MisCursos.jsx';
 import CursoAbierto from '../components/CursoAbierto/CursoAbierto.jsx';
-import FAQ from '../views/FAQ.jsx';
 
 const router = createBrowserRouter([
     {path:'/', element: <Home />},
@@ -21,8 +20,7 @@ const router = createBrowserRouter([
     {path:'/dashMensaje', element: <DashMensaje />},
     {path:'/dashboard-mensaje', element: <DashPerfil />},
     {path:'/dash-mis-cursos',element: <MisCursos/> },
-    {path:'/curso-abierto', element: <CursoAbierto />},
-    {path:'/preguntas-frecuentes', element: <FAQ />},
+    {path:'/curso-abierto', element: <CursoAbierto />}
 ]);
 
 export default router
